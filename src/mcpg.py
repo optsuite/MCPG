3d798449cb85831473158ea907a1a8cbdc66b4c8
NotformeCC <49579608+NotformeCC@users.noreply.github.com>
2023-06-27 10:15:55 +0800

    Update mcpg.py

--- src/mcpg.py
+++ src/mcpg.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2024 Cheng Chen, Ruitao Chen, Tianyou Li Zaiwen Wen
+Copyright (c) 2024 Cheng Chen, Ruitao Chen, Tianyou Li, Zaiwen Wen
 All rights reserved.
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that
 the following conditions are met:
